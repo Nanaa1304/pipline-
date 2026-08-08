54e1de1ec58f298d3778d865d6685bad31d7e59c
EL MOUHIB NAIMA <160860468+Nanaa1304@users.noreply.github.com>
2026-08-08 19:25:53 +0200

    Update minimum accuracy threshold in tests

--- tests/test_model.py
+++ tests/test_model.py
@@ -8,7 +8,7 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 
-MIN_ACCURACY = 1.5
+MIN_ACCURACY = 0.87
 
 
 def test_model_accuracy():
